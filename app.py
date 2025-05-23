9bae465830785d4706092ffc1a308e2352bff37e
Jose Mari T. Dela Pena <qjmtdelapena@tip.edu.ph>
2025-05-23 17:41:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.set_page_config(page_title="Weather Classifier", layout="centered")
 
 # -- Configuration --
 MODEL_URL = "https://drive.google.com/uc?export=download&id=1jn2DOir7LlkA9qY1ifY2STEl3FmLM09K"
-MODEL_PATH = "model.h5"
+MODEL_PATH = "weather.h5"
 IMG_SIZE = (224, 224)  # Input size for MobileNetV2
 CLASS_NAMES = ['Cloudy', 'Rainy', 'Shine', 'Sunrise']  # Update if you used different labels
 
